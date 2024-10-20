547a6dca09595652716c4f757cf0a09f39cc843f
Betty <827941041@qq.com>
2024-10-20 19:29:35 -0400

    Update take item panel ui

--- TurnBase/Assets/Scripts/UI/NumComponent.cs
+++ TurnBase/Assets/Scripts/UI/NumComponent.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NumComponent : MonoBehaviour
 {
     public TextMeshProUGUI NumLabel;
-    
+    public List<Sprite> NumImages = new List<Sprite>();
+    public Image NumImage;
     private TakeItemComponent takeItemComponent;
     private int id;
-
-
+    
     public void Setup(int num,TakeItemComponent takeItemComponent)
     {
         NumLabel.text = num.ToString();
+        NumImage.sprite = NumImages[num];
         this.takeItemComponent = takeItemComponent;
         id = num;
     }
