9e3f49e941dae27b8a68ffdccd2906cfc13fd634
Betty <827941041@qq.com>
2024-10-22 00:03:28 -0400

    Hook up menu ui with game manager

--- TurnBase/Assets/Scripts/UI/StartMenu/ChampionSelection.cs
+++ TurnBase/Assets/Scripts/UI/StartMenu/ChampionSelection.cs
@@ -1,22 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ChampionSelection : MonoBehaviour
 {
-    public bool isPlayerASelected;
-    public bool isPlayerBSelected;
 
     public void PlayerASelected()
     {
-        isPlayerASelected = true;
-        isPlayerBSelected = false;
-
+        PlayerData.IsPlayerA = true;
     }
 
     public void PlayerBSelected()
     {
-        isPlayerASelected = false;
-        isPlayerBSelected = true;
+        PlayerData.IsPlayerA = false;
     }
+}
+
+public static class PlayerData
+{
+    public static bool IsPlayerA;
 }
